558fbaba57e551694cba5e12741ea1bda30bc814
Shiblee Khan <shibleeamaan@gmail.com>
2025-11-06 22:03:28 +0530

    Add project files.

---
+++ CreditApi/CreditCard.cs
@@ -0,0 +1,10 @@
+﻿namespace CreditApi
+{
+    public class CreditCard //Class to hold credit infos
+    {
+        public string name { get; set; }
+        public string description { get; set; }
+        public int id { get; set; }
+
+    }
+}
